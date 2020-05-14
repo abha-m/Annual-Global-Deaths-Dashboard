85c4ba1ab9ff876e82fc0568024926f05fcc6a32
Abha Mutalik <abhamutalik@gmail.com>
2020-05-14 14:02:04 -0400

    tooltip for map added

--- Abzy/app.py
+++ Abzy/app.py
@@ -5,16 +5,12 @@ import numpy as np
 import json
 import pandas as pd
 
-df = pd.read_csv("data/HDI_2017.csv")
 returned_data = {}
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET"])
 def index():
-    hdi_2017 = df.to_dict(orient='records')
-    hdi_2017 = json.dumps(hdi_2017, indent=2)
-    returned_data["hdi_2017"] = hdi_2017
     return render_template("index.html", returned_data = returned_data)
     
 if __name__ == '__main__':
